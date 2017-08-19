4ce488c6f9af0d1bc2c5116036ce9b3a7306b3b2
Deependra Agrawal <deependra.agrawal@gmail.com>
2017-08-19 09:55:15 +0530

    commented setting for running locally

--- src/main/java/com/rajcorporation/tender/WebSecurityConfig.java
+++ src/main/java/com/rajcorporation/tender/WebSecurityConfig.java
@@ -90,6 +90,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	public void configure(WebSecurity web) throws Exception {
 		web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources", "/configuration/security",
 				"/swagger-ui.html", "/webjars/**");
+		//web.ignoring().antMatchers("/**");
 	}
 
 }
