39227c391972d234a7dcd5b7010efbfeb84ae43f
Personal <Personal@192.168.1.9>
2017-08-20 19:30:35 +0530

    Fixed the login issue| Had mistakenly taken out security

--- src/main/java/com/rajcorporation/tender/WebSecurityConfig.java
+++ src/main/java/com/rajcorporation/tender/WebSecurityConfig.java
@@ -79,10 +79,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		// web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui",
-		// "/swagger-resources", "/configuration/security",
-		// '' "/swagger-ui.html", "/webjars/**");
-		web.ignoring().antMatchers("/**");
+		web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources", "/configuration/security",
+				"/swagger-ui.html", "/webjars/**");
 	}
 
 }
