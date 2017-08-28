1c9e06c7cf0317e449385a580bd9bd900546098f
Karan <Karan@192.168.1.7>
2017-08-28 22:22:30 +0530

    Bypassed Lombok for the time being

--- src/main/java/com/rajcorporation/tender/security/auth/model/LoginRequest.java
+++ src/main/java/com/rajcorporation/tender/security/auth/model/LoginRequest.java
@@ -3,14 +3,29 @@
  */
 package com.rajcorporation.tender.security.auth.model;
 
-import lombok.Data;
-
 /**
  * @author Karan
  *
  */
-@Data
+
 public class LoginRequest {
 	String username;
 	String password;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 }
