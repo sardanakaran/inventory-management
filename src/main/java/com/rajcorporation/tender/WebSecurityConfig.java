1c769d73dec7f8e3cc1581c9b7074191a47a8ef6
Deependra Agrawal <deependra.agrawal@gmail.com>
2017-09-02 13:40:35 +0530

    disable security with tender api

--- src/main/java/com/rajcorporation/tender/WebSecurityConfig.java
+++ src/main/java/com/rajcorporation/tender/WebSecurityConfig.java
@@ -114,7 +114,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 		web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources", "/configuration/security",
-				"/swagger-ui.html", "/webjars/**");
+				"/swagger-ui.html", "/webjars/**", "/tender/**");
 	}
 
 	@Component
